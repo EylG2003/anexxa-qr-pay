d063ee9248134f4d6ba6cf0c83713a580b77c484
Eylem <eylem.gorgulu4@icloud.com>
2025-08-28 14:38:12 +0200

    debug: show stripe key environment

--- api/debug-env.js
+++ api/debug-env.js
@@ -1,3 +1,11 @@
-﻿module.exports = async (_req, res) => {
-  res.status(200).json({ hasStripeSecret: !!process.env.STRIPE_SECRET });
+﻿module.exports = (req, res) => {
+  const key = process.env.STRIPE_SECRET || "";
+  const env =
+    key.startsWith("sk_live_") ? "live" :
+    key.startsWith("sk_test_") ? "test" : "missing";
+  res.status(200).json({
+    hasStripeSecret: !!key,
+    stripeKeyEnv: env,
+    site: `https://${req.headers.host}`
+  });
 };
